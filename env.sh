cb788c7d2bc1711e0e6bdb9c9cb40fcdbb0e357a
bgants <bobgants@gmail.com>
2025-05-18 09:09:27 -0400

    update env file

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 # run `source env.sh` to set these before doing any CDK deployment actions
 
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
-export AWS_PRIMARY_REGION=us-east-1
-export AWS_SECONDARY_REGION=us-west-2
+export AWS_PRIMARY_REGION=
+export AWS_SECONDARY_REGION=
 
-export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/src/trade-stock:`pwd`/src/account_open:`pwd`/src/front_end
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/src/backend:`pwd`/src/front_end
 
-export AWS_DOMAIN_NAME=dungeondivinelabs.com 
+export AWS_DOMAIN_NAME=
 
 export JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1
