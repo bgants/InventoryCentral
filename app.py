542c2e39c177ee1f905da58cccdedccbe57be33d
bgants <bobgants@gmail.com>
2025-05-25 13:31:16 -0400

    modify to use CDK env vars

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 import aws_cdk as cdk
 from backend.component import Backend
 
-account = os.getenv("AWS_ACCOUNT_ID")
-primary_region = os.getenv("AWS_PRIMARY_REGION")
+
+account = os.getenv("AWS_ACCOUNT_ID") or os.getenv("CDK_DEFAULT_ACCOUNT")
+primary_region = os.getenv("AWS_PRIMARY_REGION") or os.getenv("CDK_DEFAULT_REGION")
 secondary_region = os.getenv("AWS_SECONDARY_REGION")
-domain_name = os.getenv("AWS_DOMAIN_NAME") or "example.com"
+
+domain_name = os.getenv("AWS_DOMAIN_NAME") or ""
+
 primary_environment = cdk.Environment(account=account, region=primary_region)
-# secondary_environment = cdk.Environment(account=account, region=secondary_region)
 
 app = cdk.App()
 
